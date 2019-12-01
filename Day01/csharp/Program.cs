6250391df1c80f326afe8aee9f09cde714b672f3
Christopher Suski <1329211+csuski@users.noreply.github.com>
2019-11-30 22:15:06 -0700

    solution and info for day 1

--- Day01/csharp/Program.cs
+++ Day01/csharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 
 namespace Day1
 {
@@ -6,7 +7,38 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Part 1
+            using(var reader = File.OpenText("input.dat")) {
+                var line = reader.ReadLine();
+                var total = 0;
+                while(line != null) {
+                    var val = Int32.Parse(line);
+                    val /=3;
+                    val -=2;
+                    total += val;
+                    line = reader.ReadLine();
+                }
+                Console.WriteLine($"Total = {total}");
+            }
+
+            // Part 2
+            using(var reader = File.OpenText("input.dat")) {
+                var line = reader.ReadLine();
+                var total = 0;
+                while(line != null) {
+                    var val = Int32.Parse(line);
+                    var fuel = GetFuel(val);
+                    total += fuel;
+                    line = reader.ReadLine();
+                }
+                Console.WriteLine($"Total = {total}");
+            }
+        }
+
+        public static int GetFuel(int mass) {
+            var fuel = mass / 3 - 2;
+            if(fuel <= 0) return 0;
+            return fuel + GetFuel(fuel);
         }
     }
 }
