d7e7e1cab2293c4dad7ce28ec48b4ab435606d7f
Christopher Suski <1329211+csuski@users.noreply.github.com>
2019-11-30 22:23:12 -0700

    Setup for day 2

---
+++ Day02/csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+
+        static void Part1() {
+
+        }
+
+        static void Part2() {
+
+        }
+    }
+}
