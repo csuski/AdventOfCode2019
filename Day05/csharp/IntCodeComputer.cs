c93c2e43d1f36d79cbc001ba2a3bb87c7ebb3baa
Christopher Suski <1329211+csuski@users.noreply.github.com>
2019-12-05 22:11:20 -0700

    Day5 implementation

---
+++ Day05/csharp/IntCodeComputer.cs
@@ -0,0 +1,203 @@
+using System;
+
+public class IntCodeComputer
+{
+    private IntCode _initalProgram;
+    private IntCode _memory;
+    private int _instructionPointer = 0;
+    private bool _running;
+
+    private int _input;
+    private int _output;
+
+    public int Run(IntCode program, int input)
+    {
+        _instructionPointer = 0;
+        _initalProgram = program;
+        _input = input;
+        _memory = IntCode.DeepCopy(program);
+        try
+        {
+            Run();
+        }
+        catch (Exception)
+        {
+            DumpMemory();
+            throw;
+        }
+        return _output;
+    }
+
+    private void Run()
+    {
+        _running = true;
+        while (_running)
+        {
+            ParseOpCode(_memory.State[_instructionPointer], out int operation,
+                out int paramMode1, out int paramMode2, out int paramMode3);
+
+            switch (operation)
+            {
+                case 1:
+                    Add(paramMode1, paramMode2);
+                    break;
+                case 2:
+                    Multiply(paramMode1, paramMode2);
+                    break;
+                case 3:
+                    StoreInput(_input);
+                    break;
+                case 4:
+                    _output = OutputRegister(paramMode1);
+                    break;
+                case 5:
+                    JumpIfTrue(paramMode1, paramMode2);
+                    break;
+                case 6:
+                    JumpIfFalse(paramMode1, paramMode2);
+                    break;
+                case 7:
+                    LessThanOp(paramMode1, paramMode2);
+                    break;
+                case 8:
+                    EqualsOp(paramMode1, paramMode2);
+                    break;
+                case 99:
+                    Halt();
+                    return;
+                default:
+                    throw new Exception($"Unknow OpCode: {_memory.State[_instructionPointer]}");
+            }
+        }
+    }
+
+    private void ParseOpCode(int code, out int operation, out int paramMode1,
+        out int paramMode2, out int paramMode3)
+    {
+        operation = code % 100;
+        code /= 100;
+        paramMode1 = code % 10;
+        code /= 10;
+        paramMode2 = code % 10;
+        code /= 10;
+        paramMode3 = code % 10;
+    }
+
+    private int GetValue(int address, int paramMode)
+    {
+        if (paramMode == 0)
+        {
+            return _memory.State[address];
+        }
+        else if (paramMode == 1)
+        {
+            return address;
+        }
+        throw new Exception($"Only param mode 0 and 1 are accepted. Received {paramMode}");
+    }
+
+    // Operations
+    private void Add(int paramMode1, int paramMode2)
+    {
+        // value at address 1 plus value at address 2 stored in address 3
+        int address1 = _memory.State[_instructionPointer + 1];
+        int address2 = _memory.State[_instructionPointer + 2];
+        int address3 = _memory.State[_instructionPointer + 3];
+
+        _memory.State[address3] = GetValue(address1, paramMode1) +
+            GetValue(address2, paramMode2);
+        _instructionPointer += 4;
+    }
+
+    private void Multiply(int paramMode1, int paramMode2)
+    {
+        // value at address 1 times value at address 2 stored in address 3
+        int address1 = _memory.State[_instructionPointer + 1];
+        int address2 = _memory.State[_instructionPointer + 2];
+        int address3 = _memory.State[_instructionPointer + 3];
+
+        _memory.State[address3] = GetValue(address1, paramMode1) *
+            GetValue(address2, paramMode2);
+        _instructionPointer += 4;
+    }
+
+    private void StoreInput(int input)
+    {
+        int address = _memory.State[_instructionPointer + 1];
+        _memory.State[address] = input;
+        _instructionPointer += 2;
+    }
+
+    private int OutputRegister(int paramMode)
+    {
+        int address = _memory.State[_instructionPointer + 1];
+        var output = paramMode == 0 ? _memory.State[address] : address;
+        _instructionPointer += 2;
+        return output;
+    }
+
+    private void JumpIfTrue(int paramMode1, int paramMode2)
+    {
+        Func<int, bool> isNotZero = (v) => v != 0;
+        JumpIf(paramMode1, paramMode2, isNotZero);
+    }
+
+    private void JumpIfFalse(int paramMode1, int paramMode2)
+    {
+        Func<int, bool> isZero = (v) => v == 0;
+        JumpIf(paramMode1, paramMode2, isZero);
+    }
+
+    private void JumpIf(int paramMode1, int paramMode2, Func<int, bool> func)
+    {
+        int address1 = _memory.State[_instructionPointer + 1];
+        int address2 = _memory.State[_instructionPointer + 2];
+        var val = GetValue(address1, paramMode1);
+        var val2 = GetValue(address2, paramMode2);
+
+        if (func.Invoke(val))
+        {
+            _instructionPointer = val2;
+        }
+        else
+        {
+            _instructionPointer += 3;
+        }
+    }
+
+    private void LessThanOp(int paramMode1, int paramMode2)
+    {
+        Func<int, int, bool> lessThan = (v1, v2) => v1 < v2;
+        StoreComparison(paramMode1, paramMode2, lessThan);
+    }
+
+    private void EqualsOp(int paramMode1, int paramMode2)
+    {
+        Func<int, int, bool> equals = (v1, v2) => v1 == v2;
+        StoreComparison(paramMode1, paramMode2, equals);
+    }
+
+    private void StoreComparison(int paramMode1, int paramMode2, Func<int, int, bool> func)
+    {
+        int address1 = _memory.State[_instructionPointer + 1];
+        int address2 = _memory.State[_instructionPointer + 2];
+        int address3 = _memory.State[_instructionPointer + 3];
+        var val = GetValue(address1, paramMode1);
+        var val2 = GetValue(address2, paramMode2);
+        _memory.State[address3] = func.Invoke(val, val2) ? 1 : 0;
+        _instructionPointer += 4;
+    }
+
+    private void Halt()
+    {
+        _running = false;
+        _instructionPointer += 1;
+    }
+
+    private void DumpMemory()
+    {
+        Console.WriteLine($"Current Memory: {_memory.ToString()}");
+        Console.WriteLine($"Current Instruction: {_instructionPointer}");
+        Console.WriteLine($"Running : {_running.ToString()}");
+    }
+}
--- Day05/csharp/Program.cs
+++ Day05/csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace csharp
 {
@@ -6,7 +7,61 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0 && args[0].Equals("Part1",
+              StringComparison.CurrentCultureIgnoreCase))
+            {
+                Part1(File.ReadAllText("input.txt"));
+
+            }
+            else if (args.Length > 0 && args[0].Equals("Part2",
+              StringComparison.CurrentCultureIgnoreCase))
+            {
+                Part2(File.ReadAllText("input.txt"));
+            }
+            else
+            {
+                Test();
+            }
+        }
+
+        static string[] tests = new string[] {
+            "3,9,8,9,10,9,4,9,99,-1,8",
+            "3,9,7,9,10,9,4,9,99,-1,8",
+            "3,3,1108,-1,8,3,4,3,99",
+            "3,3,1107,-1,8,3,4,3,99",
+            "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9",
+            "3,3,1105,-1,9,1101,0,0,12,4,12,99,1",
+            "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+            };
+
+        static void Test()
+        {
+            Console.WriteLine("TESTING...");
+            var computer = new IntCodeComputer();
+            foreach (var test in tests)
+            {
+                var t = new IntCode(test);
+                Console.WriteLine($"Input = {t.ToString()}");
+                var result = computer.Run(t, 9);
+                Console.WriteLine($"Output = {result.ToString()}");
+            }
+        }
+
+        static void Part1(string text)
+        {
+            Console.WriteLine("Part1...");
+            var computer = new IntCodeComputer();
+            var program = new IntCode(text);
+            var result = computer.Run(program, 1);
+            Console.WriteLine(result.ToString());
+        }
+        static void Part2(string text)
+        {
+            Console.WriteLine("Part2...");
+            var computer = new IntCodeComputer();
+            var program = new IntCode(text);
+            var result = computer.Run(program, 5);
+            Console.WriteLine(result.ToString());
         }
     }
 }
