9e856eae112fb241bcdb9344ca65ebc79aa22cf9
Christopher Suski <1329211+csuski@users.noreply.github.com>
2019-12-07 22:35:27 -0700

    Quick Day 8 implementation

--- Day08/csharp/Program.cs
+++ Day08/csharp/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -8,74 +9,71 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0 && args[0].Equals("Part1",
-              StringComparison.CurrentCultureIgnoreCase))
-            {
-                Part1(File.ReadAllText("input.txt"));
-
-            }
-            else if (args.Length > 0 && args[0].Equals("Part2",
-              StringComparison.CurrentCultureIgnoreCase))
-            {
-                Part2(File.ReadAllText("input.txt"));
-            }
-            else
-            {
-                Test(testsPart1);
-                Test(testsPart2);
-            }
+            var array = Part1(File.ReadAllText("input.txt"));
+            Part2(array);
         }
 
-        public class TestItem
-        {
-            public string Input;
-            public string Result;
+        const int Width = 25;
+        const int Height = 6;
+        const int Pixels = Width * Height;
 
-        }
-
-        static TestItem[] testsPart1 = new TestItem[] {
-            new TestItem() {
-                Input = "",
-                Result = ""
-            }
-        };
-
-        static TestItem[] testsPart2 = new TestItem[] {
-            new TestItem() {
-                Input = "",
-                Result = ""
-            }
-        };
-
-        static void Test(TestItem[] testInput)
-        {
-            Console.WriteLine("TESTING...");
-            foreach (var test in testInput)
-            {
-                if (!test.Input.Equals(test.Result))
-                {
-                    Console.WriteLine("***TEST FAILED***");
-                    Console.WriteLine($"Input: {test.Input}");
-                    Console.WriteLine($"Expected Result: {test.Result}");
-                }
-                else
-                {
-                    Console.WriteLine("***Test Passed***");
-                }
-            }
-        }
-
-        static void Part1(string text)
+        static List<int[]> Part1(string text)
         {
             Console.WriteLine("Part1...");
+            List<int[]> layers = new List<int[]>();
+            var allVals = text.Select(c => Int32.Parse(c.ToString())).ToArray();
+            int layerCount = allVals.Length / Pixels;
+            for (int i = 0; i < layerCount; i++)
+            {
+                var layer = new int[Pixels];
+                Array.Copy(allVals, i * Pixels, layer, 0, Pixels);
+                layers.Add(layer);
+            }
 
+            int[] fewestZeroLayer = layers[0];
+            int fewestZeros = fewestZeroLayer.Count(x => x == 0);
+            for(int i = 1; i < layers.Count; i++)
+            {
+                var zeros = layers[i].Count(x => x == 0);
+                if (zeros < fewestZeros)
+                {
+                    fewestZeros = zeros;
+                    fewestZeroLayer = layers[i];
+                }
+            }
 
+            var ones = fewestZeroLayer.Count(x => x == 1);
+            var twos = fewestZeroLayer.Count(x => x == 2);
+            Console.WriteLine(ones * twos);
+            return layers;
         }
 
-        static void Part2(string text)
+        static void Part2(List<int[]> layers)
         {
             Console.WriteLine("Part2...");
+            int[] masterLayer = new int[Pixels];
+            Array.Copy(layers[0], masterLayer, Pixels);
+            for(int l = 1; l < layers.Count; l++)
+            {
+                for (int i = 0; i < Pixels; i++)
+                {
+                    if (masterLayer[i] == 2)
+                    {
+                        masterLayer[i] = layers[l][i];
+                    }
+                }
+            }
 
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    var va = masterLayer[i * Width + j];
+                    if (va == 1) { Console.Write("*"); }
+                    else { Console.Write(" "); }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
